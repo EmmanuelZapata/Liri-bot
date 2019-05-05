5b579d8c229a954c779ef699e0f23d0537860952
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-05-04 21:00:09 -0500

    OMDB works now... it really does

--- liri.js
+++ liri.js
@@ -27,30 +27,59 @@ var moment = require("moment");
 
 //Bands in Town
 
-var artist = process.argv[2];
+// var artist = process.argv[2];
 
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-    function(response) {
-      // If the axios was successful...
-      // Then log the body from the site!
-      console.log(response.data);
-    },
+// axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+//     function(response) {
+//       // If the axios was successful...
+//       // Then log the body from the site!
+//       console.log(response.data);
+//     },
   
-    function(error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    }
-  );
+//     function(error) {
+//       if (error.response) {
+//         // The request was made and the server responded with a status code
+//         // that falls out of the range of 2xx
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//       } else if (error.request) {
+//         // The request was made but no response was received
+//         // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//         console.log(error.request);
+//       } else {
+//         // Something happened in setting up the request that triggered an Error
+//         console.log("Error", error.message);
+//       }
+//       console.log(error.config);
+//     }
+//   );
+
+var movies = process.argv[2];
+axios.get("http://www.omdbapi.com/?t=" + movies + "&y=&plot=short&apikey=9426f43d").then(
+        function(response) {
+          // If the axios was successful...
+          // Then log the body from the site!
+          console.log(response.data);
+        },
+      
+        function(error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        }
+      );
+
+
