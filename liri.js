f974afc1a17fa77c3ddd936b9432c4a7328f58cd
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-05-11 18:33:47 -0500

    got concert-this command to work need to work on making the output look nice

--- liri.js
+++ liri.js
@@ -36,6 +36,14 @@ switch(command){
   else {spotifySong("Fear the Reaper");
   }
   break;
+
+  case "concert-this":
+  if (x){
+    bandTown(x);
+  }
+  else {bandTown("none");
+  }
+  break;
 }
 
 
@@ -54,33 +62,34 @@ function spotifySong (song) {
 
 //Bands in Town
 
-// var artist = process.argv[2];
+function bandTown (artist) {
 
-// axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-//     function(response) {
-//       // If the axios was successful...
-//       // Then log the body from the site!
-//       console.log(response.data);
-//     },
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+    function(response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data);
+    },
   
-//     function(error) {
-//       if (error.response) {
-//         // The request was made and the server responded with a status code
-//         // that falls out of the range of 2xx
-//         console.log(error.response.data);
-//         console.log(error.response.status);
-//         console.log(error.response.headers);
-//       } else if (error.request) {
-//         // The request was made but no response was received
-//         // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//         console.log(error.request);
-//       } else {
-//         // Something happened in setting up the request that triggered an Error
-//         console.log("Error", error.message);
-//       }
-//       console.log(error.config);
-//     }
-//   );
+    function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    }
+  );
+}
 
 // var movies = process.argv[2];
 // axios.get("http://www.omdbapi.com/?t=" + movies + "&y=&plot=short&apikey=9426f43d").then(
