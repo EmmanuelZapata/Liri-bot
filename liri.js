8f77f90063def33f70a968dd15e8e882c3ad90aa
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-05-11 19:09:17 -0500

    got the movies to display correctly

--- liri.js
+++ liri.js
@@ -47,10 +47,13 @@ switch(command){
   if (x){
     movieTime(x);
   }
-  else console.log("Please enter a movie or this just won't work!");
+  else {movieTime("Mr. Nobody");
+  }
   break;
 }
 
+
+//Spotify
 
 function spotifySong (song) {
     spotify.search({ type: 'track', query: song}, function(err, data) {
@@ -96,32 +99,51 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
   );
 }
 
+// OMDB 
 
 function movieTime (movies) {
 axios.get("http://www.omdbapi.com/?t=" + movies + "&y=&plot=short&apikey=9426f43d").then(
         function(response) {
           // If the axios was successful...
           // Then log the body from the site!
-          console.log(response.data);
-        },
-      
-        function(error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
+          //  * Title of the movie.
+          // * Year the movie came out.
+          // * IMDB Rating of the movie.
+          // * Rotten Tomatoes Rating of the movie.
+          // * Country where the movie was produced.
+          // * Language of the movie.
+          // * Plot of the movie.
+          // * Actors in the movie.
+          console.log("\n----Movie----");
+          console.log("Title Of Movie: " + response.data.Title);
+          console.log("Year The Movie Came Out: " + response.data.Year);
+          console.log("IMDB Rating Of Movie: " + response.data.imdbRating);
+          console.log("Rotten Tomates Rating Of Movie: " + response.data.tomatoRating);
+          console.log("Country Where The Movie Was Produced: " + response.data.Country);
+          console.log("Language Of The Movie: " + response.data.Language);
+          console.log("Plot Of The Movie: " + response.data.Plot);
+          console.log("Actors In The Movie: " + response.data.Actors);
         }
-      );
+);
       }
+      
+      //   function(error) {
+      //     if (error.response) {
+      //       // The request was made and the server responded with a status code
+      //       // that falls out of the range of 2xx
+      //       console.log(error.response.data);
+      //       console.log(error.response.status);
+      //       console.log(error.response.headers);
+      //     } else if (error.request) {
+      //       // The request was made but no response was received
+      //       // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      //       console.log(error.request);
+      //     } else {
+      //       // Something happened in setting up the request that triggered an Error
+      //       console.log("Error", error.message);
+      //     }
+      //     console.log(error.config);
+      //   }
+      // );
+      // }
 
